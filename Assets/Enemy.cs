cf671b70e09655d505b9db23e7ff1be34ee04bbf
chentaolu <chentaolu@mail.fcu.edu,tw>
2021-06-12 17:37:02 +0800

    add  level and enemy model and dance

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -3,14 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
-    public 
+
+    public Level level = new Level();
+    public int nowBlood;
 	// Use this for initialization
 	void Start () {
-		
+        this.nowBlood = 100 * level.nowLevel;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(this.isDead())
+        {
+            level.nextLevel();
+            updateBlood();
+        }
+        else
+        {
+
+        }
 	}
+
+    public bool isDead()
+    {
+        if (nowBlood <= 0)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    public void updateBlood()
+    {
+        this.nowBlood = 100 * level.nowLevel;
+    }
+
+
 }
---
+++ Assets/Level.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    public int nowLevel = 1;
+
+    public void nextLevel ()
+    {
+        this.nowLevel = this.nowLevel + 1;
+    }
+}
