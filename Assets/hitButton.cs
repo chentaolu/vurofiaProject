f3607ee0df887ee1917681aea4af45722c32bf6c
GaWei <x981235xyvw@gmail.com>
2021-06-13 01:44:42 +0800

    health bar fix

--- Assets/hitButton.cs
+++ Assets/hitButton.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class hitButton : MonoBehaviour {
     public Enemy enemy;
+    public health health;
     public void OnBtn()
     {
         enemy.beHit = true;
         enemy.nowBlood = enemy.nowBlood - 25;
+        enemy.precentBlood = enemy.precentBlood - ((25 / enemy.maxBlood) * 100);
+        
     }
 }
