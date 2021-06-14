c1dd199e46aaba982e73fe3de689cbf86a436947
GaWei <x981235xyvw@gmail.com>
2021-06-14 21:49:42 +0800

    Merge remote-tracking branch 'origin/test' into test

---
+++ Assets/levelBoard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class levelBoard : MonoBehaviour {
+	public Text showLevel;
+	public int enemyLevel;
+
+	// Use this for initialization
+	void Start () {
+		this.enemyLevel = Level.nowLevel;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.enemyLevel = Level.nowLevel;
+		showLevel.text = enemyLevel.ToString();
+	}
+}
