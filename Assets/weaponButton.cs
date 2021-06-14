e4caacee7ca412aeba9b42ce069c92ad293db770
YunHaoW <d0786880@mail.fcu.edu.tw>
2021-06-14 16:00:02 +0800

    fix weapon system

--- Assets/weaponButton.cs
+++ Assets/weaponButton.cs
@@ -9,10 +9,27 @@ public class weaponButton : MonoBehaviour
 	public Sprite targetImage;
 	public List<Sprite> spritesList;
 	public int index = 0;
+	public PlayerScript playerScript;
+	public KnifeConfig knifeConfig;
+	public Ak_47Config ak_47Config;
+	public M4A1Config m4A1Config;
+	public SkorpionVZConfig skorpionVZConfig;
+	public UMP45Config uMP45Config;
+
+	Sprite Knife;
+	Sprite UMP_45;
+	Sprite Skorpion_VZ;
+	Sprite M4A1;
+	Sprite Ak_47;
 
 	// Use this for initialization
 	void Start()
 	{
+		Knife = Resources.Load<Sprite>("weapon/Knife");
+		UMP_45 = Resources.Load<Sprite>("weapon/UMP-45");
+		Skorpion_VZ = Resources.Load<Sprite>("weapon/Skorpion_VZ");
+		M4A1 = Resources.Load<Sprite>("weapon/M4A1");
+		Ak_47 = Resources.Load<Sprite>("weapon/Ak-47");
 		initSpritesList();
 		targetImage = spritesList[0];
 		button = GameObject.Find("WeaponButton");
@@ -21,40 +38,79 @@ public class weaponButton : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		updateSpritesList();
 		button.GetComponent<Image>().sprite = targetImage;
 	}
 
 	void initSpritesList()
 	{
-		Sprite weapon0 = Resources.Load<Sprite>("weapon/Knife");
-		Sprite weapon1 = Resources.Load<Sprite>("weapon/UMP-45");
-		Sprite weapon2 = Resources.Load<Sprite>("weapon/Skorpion_VZ");
-		Sprite weapon3 = Resources.Load<Sprite>("weapon/M4A1");
-		Sprite weapon4 = Resources.Load<Sprite>("weapon/Ak-47");
-		Sprite weapon5 = Resources.Load<Sprite>("weapon/Smoke_Grenade");
-		Sprite weapon6 = Resources.Load<Sprite>("weapon/ACOG_Sight");
-		spritesList.Add(weapon0);
-		spritesList.Add(weapon1);
-		spritesList.Add(weapon2);
-		spritesList.Add(weapon3);
-		spritesList.Add(weapon4);
-		spritesList.Add(weapon5);
-		spritesList.Add(weapon6);
+		spritesList.Clear();
+		spritesList.Add(Knife);
 	}
+
+	void updateSpritesList()
+    {
+		List<WeaponConfig> canUseList = playerScript.getCanUseList();
+		if (canUseList.Count != 0)
+        {
+			for(int i = 0; i < canUseList.Count; i++)
+			{
+				if (canUseList[i].GetType().Equals(typeof(M4A1Config)))
+                {
+					spritesList.Add(M4A1);
+                }
+				else if (canUseList[i].GetType().Equals(typeof(Ak_47Config)))
+                {
+					spritesList.Add(Ak_47);
+                }
+				else if (canUseList[i].GetType().Equals(typeof(UMP45Config)))
+                {
+					spritesList.Add(UMP_45);
+                }
+				else if (canUseList[i].GetType().Equals(typeof(SkorpionVZConfig)))
+                {
+					spritesList.Add(Skorpion_VZ);
+                }
+			}
+        }
+    }
 
 	public void changeItems()
 	{
 		index++;
-		if (index > Level.nowLevel)
+		if (index > spritesList.Count - 1)
         {
 			index = 0;
         }
+
 		targetImage = spritesList[index];
+
 		if (index == 0)
         {
 			changeButton.changeIcon(0);
         } else {
 			changeButton.changeIcon(1);
+        }
+
+		if (spritesList[index] == Knife)
+		{
+			playerScript.changeWeapon(knifeConfig);
+		}
+		else if (spritesList[index] == Ak_47)
+		{
+			playerScript.changeWeapon(ak_47Config);
+		}
+		else if (spritesList[index] == UMP_45)
+		{
+			playerScript.changeWeapon(uMP45Config);
+		}
+		else if (spritesList[index] == M4A1)
+        {
+			playerScript.changeWeapon(m4A1Config);
+        }
+		else if (spritesList[index] == Skorpion_VZ)
+        {
+			playerScript.changeWeapon(skorpionVZConfig);
         }
 	}
 }
